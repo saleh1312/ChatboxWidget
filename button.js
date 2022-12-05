7ea62a913a4ec0881ad525e9217cf25f4293a8ce
Mohamed Abd El-Monem <43797096+MuhamedAhmed11@users.noreply.github.com>
2022-12-05 21:24:08 +0200

    Update button.js

--- button.js
+++ button.js
@@ -1,1 +1,11 @@
 document.body.style.backgroundColor = "orange"
+class ButtonJs extends HTMLElement {
+    constructor() {
+        super();
+    }
+}
+
+customElements.define('button-js', ButtonJs);
+var button = new ButtonJs();
+button.innerHTML = "<button>Test Button</button>"
+document.body.appendChild(button);
