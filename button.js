8734b21dfab2853052639dec3cb611798a517841
MuhamedAhmed11 <ma91463@icloud.com>
2022-12-07 02:03:54 +0200

    changes

--- button.js
+++ button.js
@@ -1,10 +1,29 @@
-let myElm = document.createElement("button");	
+//  Chat Widget Button
+let chatbutton = document.createElement('button');
+chatbutton.className = 'open-button';
+chatbutton.onclick = 'openForm()';
+document.body.appendChild(chatbutton);
 
-myElm.innerText = 'Test';		
-myElm.style.color = 'red';		
-myElm.style.position = 'absolute';
-myElm.style.right = '5%';
-myElm.style.bottom = '5%';
-myElm.style.zIndex = '1000000';
+//
+let chatWidget = document.createElement('div');
+chatWidget.className = 'chat-popup';
 
-document.body.appendChild(myElm);
+//
+let chatForm = document.createElement('from');
+chatForm.className = 'form-container';
+let header1 = document.createElement('h1');
+header1.innerText('Chat');
+
+//
+chatForm.append(header1);
+//
+chatWidget.appendChild(chatForm);
+
+// myElm.innerText = 'Test';
+// myElm.style.color = 'red';
+// myElm.style.position = 'absolute';
+// myElm.style.right = '5%';
+// myElm.style.bottom = '5%';
+// myElm.style.zIndex = '1000000';
+
+document.body.appendChild(chatWidget);
